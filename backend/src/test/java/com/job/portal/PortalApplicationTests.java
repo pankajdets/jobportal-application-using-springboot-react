4412e3110abb1af3723f77265e4503baaa2edcc4
Pankaj Ray <panray@publicisgroupe.net>
2022-07-05 23:09:30 +0530

    first commit

---
+++ backend/src/test/java/com/job/portal/PortalApplicationTests.java
@@ -0,0 +1,10 @@
+package com.job.portal;
+
+
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalApplicationTests {
+
+
+}
---
+++ backend/start.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+java -jar /jobportal-app/target/portal-0.0.1-SNAPSHOT.jar
